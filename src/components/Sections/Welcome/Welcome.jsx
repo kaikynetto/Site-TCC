2b3b35504464fcdcfeb6270bd62b9f1c0cac9b35
kaikynetto <kaikydiasnetto21@gmail.com>
2023-04-24 12:49:11 -0300

    hero

--- src/components/Sections/Welcome/Welcome.jsx
+++ src/components/Sections/Welcome/Welcome.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Column1, Column2, HeroContainer, HeroRow, HeroWrapper, TextWrapper } from './WelcomeElements'
+import { Column1, Column2, Heading, HeroContainer, HeroRow, HeroWrapper, ImgWrap, Subtitle, TextWrapper } from './WelcomeElements'
+import img from "../../../../public/imgs/heroImage.png"
+import Image from 'next/image'
 
 export default function Welcome() {
   return (
@@ -8,11 +10,14 @@ export default function Welcome() {
         <HeroRow>
           <Column1>
             <TextWrapper>
-              
+              <Heading>A flecha dourada dos freelancers</Heading>
+              <Subtitle>A holding líder em tecnologia por trás do aplicativo Tasking conectando talentos ao sucesso.</Subtitle>
             </TextWrapper>
           </Column1>
           <Column2>
-
+            <ImgWrap>
+              <Image width="200px" height="450px" src={img}/>
+            </ImgWrap>
           </Column2>
         </HeroRow>
       </HeroWrapper>
--- src/components/Sections/Welcome/WelcomeElements.jsx
+++ src/components/Sections/Welcome/WelcomeElements.jsx
@@ -40,6 +40,9 @@ export const Column1 = styled.div`
 export const Column2 = styled.div`
   margin-bottom: 15px;
   padding: 0 15px;
+  align-items:center;
+  justify-content: center;
+  align-content: center;
   grid-area: col2;
 `;
 
@@ -47,4 +50,33 @@ export const TextWrapper = styled.div`
   max-width: 540px;
   padding-top: 0;
   padding-bottom: 16px;
+`;
+
+export const Heading = styled.h1`
+  color: #b79950;
+  // text-transform: uppercase;
+`;
+
+export const Subtitle = styled.h1`
+  color: white;
+  opacity: 0.6;
+  font-weight: 200;
+  font-size: 22px;
+  // text-transform: uppercase;
+`;
+
+export const ImgWrap = styled.div`
+  max-width: 555px;
+  height: 100%;
+  align-items:center;
+  justify-content: center;
+  align-content: center;
+  margin-left: 25%;
+  margin-top: 10px;
+`;
+
+export const Img = styled.img`
+  width: 100%;
+  margin: 0 0 10px 0;
+  padding-right: 0;
 `;
